f7a81258d697c8b48e1f1d82ed3fc409b880007e
Michael McGuire <mmcguire@fdbhealth.com>
2021-11-24 14:17:41 -0500

    before cleaning up file for org-roam

--- custom.el
+++ custom.el
@@ -9,10 +9,9 @@
    ["#FAFAFA" "#FF1744" "#66BB6A" "#F57F17" "#42A5F5" "#7E57C2" "#0097A7" "#546E7A"])
  '(ansi-term-color-vector
    [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"] t)
- '(compilation-message-face (quote default))
+ '(compilation-message-face 'default)
  '(dired-filter-saved-filters
-   (quote
-    (("clean_view"
+   '(("clean_view"
       (not
        (regexp . ".*~$"))
       (not
@@ -24,36 +23,31 @@
      (#("no underscore" 0 1
         (idx 0))
       (not
-       (regexp . "^_.*"))))))
+       (regexp . "^_.*")))))
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(magit-diff-use-overlays nil)
  '(markdown-asymmetric-header t)
  '(markdown-command "/usr/local/bin/markdown")
- '(markdown-live-preview-delete-export (quote delete-on-export))
+ '(markdown-live-preview-delete-export 'delete-on-export)
  '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
  '(org-show-context-detail
-   (quote
-    ((occur-tree . minimal)
+   '((occur-tree . minimal)
      (agenda . local)
      (bookmark-jump . lineage)
      (isearch . lineage)
-     (default . minimal))))
+     (default . minimal)))
  '(package-selected-packages
-   (quote
-    (magit smartparens request realgud py-autopep8 lsp-python-ms elpy ox-gfm company-jedi flycheck lsp- company-lsp jedi counsel-etags lsp-mode dired-collapse dired-subtree quelpa-use-package quelpa projectile-ripgrep iedit occidental-theme professional-theme multiple-cursors greymatters-theme spacemacs-theme eziam-theme leuven-theme autumn-light-theme delight hc-zenburn-theme labburn-theme yasnippet solarized-theme github-theme terraform=mode company-terraform terraform-mode hcl-mode color-theme-modern color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme-solarized paredit vimish-fold flatui-theme github-modern-theme minimal-theme undo-tree json-mode yaml-mode worf markdown-mode lua-mode avy zenburn-theme htmlize highlight-indent-guides origami makey discover indent-tools dired-ranger ranger el-get command-log-mode base16-twilight base16-twilight-theme twilight twilight-anti-bright-theme twilight-bright-theme twilight-theme tango-plus-theme tango-plus apropospriate-theme moe-theme base16-theme gruvbox-theme chruby seeing-is-believing ruby-electric dired-filter dired-narrow rainbow-delimiters robe company company-shell wgrep wgrep-ack wgrep-ag ztree ivy-hydra org counselq counsel-osx-app counsel-projectile highlight-indentation company-restclient restclient test-kitchen ag json-reformat smartscan which-key smooth-scrolling smooth-scroll peep-dired org-projectile with-editor session magit-popup hydra helm git-gutter+ git-commit fringe-helper epl diminish dash bind-key avys async ace-link window-number whole-line-or-region use-package simpleclip rebox2 powershell powerline pkg-info pdf-tools neotree git-gutter-fringe+ expand-region exec-path-from-shell dired-toggle-sudo dired+ deft cl-lib-highlight boxquote ace-window)))
- '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
+   '(org-roam magit smartparens request realgud py-autopep8 lsp-python-ms elpy ox-gfm company-jedi flycheck lsp- company-lsp jedi counsel-etags lsp-mode dired-collapse dired-subtree quelpa-use-package quelpa projectile-ripgrep iedit occidental-theme professional-theme multiple-cursors greymatters-theme spacemacs-theme eziam-theme leuven-theme autumn-light-theme delight hc-zenburn-theme labburn-theme yasnippet solarized-theme github-theme terraform=mode company-terraform terraform-mode hcl-mode color-theme-modern color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme-solarized paredit vimish-fold flatui-theme github-modern-theme minimal-theme undo-tree json-mode yaml-mode worf markdown-mode lua-mode avy zenburn-theme htmlize highlight-indent-guides origami makey discover indent-tools dired-ranger ranger el-get command-log-mode base16-twilight base16-twilight-theme twilight twilight-anti-bright-theme twilight-bright-theme twilight-theme tango-plus-theme tango-plus apropospriate-theme moe-theme base16-theme gruvbox-theme chruby seeing-is-believing ruby-electric dired-filter dired-narrow rainbow-delimiters robe company company-shell wgrep wgrep-ack wgrep-ag ztree ivy-hydra org counselq counsel-osx-app counsel-projectile highlight-indentation company-restclient restclient test-kitchen ag json-reformat smartscan which-key smooth-scrolling smooth-scroll peep-dired org-projectile with-editor session magit-popup hydra helm git-gutter+ git-commit fringe-helper epl diminish dash bind-key avys async ace-link window-number whole-line-or-region use-package simpleclip rebox2 powershell powerline pkg-info pdf-tools neotree git-gutter-fringe+ expand-region exec-path-from-shell dired-toggle-sudo dired+ deft cl-lib-highlight boxquote ace-window))
+ '(pdf-view-midnight-colors '("#969896" . "#f8eec7"))
  '(show-paren-mode t)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
  '(tool-bar-mode nil)
  '(vc-annotate-background-mode nil)
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
+   '(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))
  '(xterm-color-names
    ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
  '(xterm-color-names-bright
